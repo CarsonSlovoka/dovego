955bf5352528c5906f5cccfd11ba0792dfb2edb6
Carson <jackparadise520a@gmail.com>
2021-09-24 17:55:47 +0800

    Create a server With Golang (#2)

---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "fmt"
+    "github.com/CarsonSlovoka/dovego/app"
+    "github.com/CarsonSlovoka/dovego/app/log"
+    "github.com/CarsonSlovoka/dovego/app/server"
+    "github.com/CarsonSlovoka/dovego/app/urls"
+    "os/exec"
+)
+
+func main() {
+    file := log.InitLog("dovego.temp.log")
+    if file != nil {
+        defer func() {
+            log.Trace.Printf("Exit App.")
+            _ = file.Close()
+        }()
+    }
+    app.LoadConfig()
+
+    quit := make(chan bool)
+    port := app.Config.Server.Port
+    go func() {
+        urls.InitURLs()
+        if err := server.ListenAndServe(port); err != nil {
+            log.Trace.Println(err)
+        }
+        quit <- true
+    }()
+
+    rootURL := fmt.Sprintf("http://localhost:%d", port)
+    go func() {
+        if err := exec.Command("rundll32", "url.dll,FileProtocolHandler",
+            rootURL,
+        ).Start(); err != nil {
+            panic(err)
+        }
+    }()
+
+    for {
+        select {
+        case <-quit:
+            // log.Println("Close App.")
+            return
+        }
+    }
+}
