0280b8e65d3069276fbd0ef2bd707a17165996c6
Carson <jackparadise520a@gmail.com>
2021-10-22 13:40:21 +0800

    Add start_url for the user to set the homepage. (#5)

--- src/app/config/server.go
+++ src/app/config/server.go
@@ -3,6 +3,7 @@ package config
 type Server struct {
     Port int `json:"port"`
     Shutdown `json:"shutdown"`
+    StartURL string `json:"start_url"`
 }
 
 type Shutdown struct {
--- src/app/urls/static.go
+++ src/app/urls/static.go
@@ -1,13 +1,16 @@
 package urls
 
 import (
-    "embed"
-    _ "embed"
-    "github.com/CarsonSlovoka/dovego/app/log"
-    "github.com/CarsonSlovoka/dovego/app/server"
-    http2 "github.com/CarsonSlovoka/dovego/pkg/net/http"
+	"embed"
+	_ "embed"
+	"github.com/CarsonSlovoka/dovego/app"
+	"github.com/CarsonSlovoka/dovego/app/server"
+	http2 "github.com/CarsonSlovoka/dovego/pkg/net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "net/http"
+	"net/http"
 )
 
 //go:embed static/* tmpl/*
@@ -17,16 +20,29 @@ var systemResourceFS embed.FS
 var rootHTMLFS embed.FS
 
 func initSystemStaticResource() {
-    server.Mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path == "/" {
-            bytes, err := rootHTMLFS.ReadFile("tmpl/index.html")
-            if err != nil {
-                log.Error.Printf(err.Error())
-            }
-            _, _ = w.Write(bytes)
-            return
-        }
+	server.Mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-        http2.FileServer(http.FS(systemResourceFS)).ServeHTTP(w, r)
-    })
+		startURL := app.Config.Server.StartURL
+		if r.URL.Path == "/" && startURL != "" {
+
+			if _, err := os.Stat(startURL); os.IsNotExist(err) || strings.ToLower(filepath.Ext(startURL)) != ".html" {
+				http2.ErrorWithHTML(w, err.Error(), http.StatusNotFound)
+				return
+			}
+
+			http.ServeFile(w, r, app.Config.Server.StartURL)
+
+			/*
+				bytes, err := rootHTMLFS.ReadFile(app.Config.Server.StartURL)
+				if err != nil {
+					http2.ErrorWithHTML(w, err.Error(), http.StatusForbidden)
+					return
+				}
+				_, _ = w.Write(bytes)
+			*/
+			return
+		}
+
+		http2.FileServer(http.FS(systemResourceFS)).ServeHTTP(w, r)
+	})
 }
